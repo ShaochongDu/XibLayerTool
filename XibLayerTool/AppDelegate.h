a4d342c950a5b580f3206eca4419ba1a6482000d
dushaochong <dushaochong@icloud.com>
2016-08-19 10:21:28 +0800

    Initial Commit

---
+++ XibLayerTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XibLayerTool
+//
+//  Created by Shaochong Du on 16/8/19.
+//  Copyright © 2016年 Shaochong Du. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XibLayerTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XibLayerTool
+//
+//  Created by Shaochong Du on 16/8/19.
+//  Copyright © 2016年 Shaochong Du. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
