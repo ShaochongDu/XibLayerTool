77ac622cd3c1531026c32360d5755ed0123d7f37
dushaochong <dushaochong@icloud.com>
2016-08-19 11:11:25 +0800

    增加代码修改layer属性

--- XibLayerTool/ViewController.m
+++ XibLayerTool/ViewController.m
@@ -24,16 +24,26 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    //  获取xib中已设置的layer属性
     NSLog(@"\n ------- myView xib property --------\n xibMaskToBounds->%d \n xibCornerRadius-> %.1f \n xibBorderWidth->%.1f\n xibBorderColor->%@",self.myView.xibMaskToBounds,self.myView.xibCornerRadius,self.myView.xibBorderWidth,self.myView.xibBorderColor);
     
     NSLog(@"\n ------- myBtn xib property --------\n xibMaskToBounds->%d \n xibCornerRadius-> %.1f \n xibBorderWidth->%.1f\n xibBorderColor->%@",self.myBtn.xibMaskToBounds,self.myBtn.xibCornerRadius,self.myBtn.xibBorderWidth,self.myBtn.xibBorderColor);
     
     NSLog(@"\n ------- myTextView xib property --------\n xibMaskToBounds->%d \n xibCornerRadius-> %.1f \n xibBorderWidth->%.1f\n xibBorderColor->%@",self.myTextView.xibMaskToBounds,self.myTextView.xibCornerRadius,self.myTextView.xibBorderWidth,self.myTextView.xibBorderColor);
     
+    //  修改属性值
+    self.myView.xibCornerRadius = arc4random()%60;
+    CGFloat randomRed = arc4random()%255/255.0;
+    CGFloat randomGreen = arc4random()%255/255.0;
+    CGFloat randomBlue = arc4random()%255/255.0;
+    self.myView.xibBorderColor = [UIColor colorWithRed:randomRed green:randomGreen blue:randomBlue alpha:0.5];
+    
     //  自定义button 打印取值会有问题 没有设置get方法
     //  需要使用系统方法获取
 //    NSLog(@"\n ------- customerBtn xib property --------\n xibMaskToBounds->%d \n xibCornerRadius-> %.1f \n xibBorderWidth->%.1f\n xibBorderColor->%@",self.customerBtn.xibMaskToBounds,self.customerBtn.xibCornerRadius,self.customerBtn.xibBorderWidth,self.customerBtn.xibBorderColor);
     NSLog(@"\n ------- customerBtn xib property --------\n xibMaskToBounds->%d \n xibCornerRadius-> %.1f \n xibBorderWidth->%.1f\n xibBorderColor->%@",self.customerBtn.layer.masksToBounds,self.customerBtn.layer.cornerRadius,self.customerBtn.layer.borderWidth,self.customerBtn.layer.borderColor);
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
